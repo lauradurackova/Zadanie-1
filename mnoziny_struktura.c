f9ececcc293ea43112395442443ca74383247b5d
lauradurackova <48187852+lauradurackova@users.noreply.github.com>
2019-05-13 09:51:22 +0200

    Funkcia zjednotenie + volanie
    Vytvorenie funkcie na zjednotenie mnozin a jej volanie

--- mnoziny_struktura.c
+++ mnoziny_struktura.c
@@ -28,6 +28,49 @@ void prienik(MNC* a, MNC* b, MNC* c)
 	}
 }
 
+void zjednotenie(MNC* a, MNC* b, MNC* d)
+{
+	int i,j=0,n=0,h=0;
+	
+	for(i=0;i<6;i++)
+	{
+		while(d->pole[j]!=0)
+		{
+			if(a->pole[i]==d->pole[j])
+				n=1;
+			j++;
+		}
+		if(n==0)
+		{
+			d->pole[j]=a->pole[i];
+			h++;
+		}
+	}
+	
+	for(i=0;i<6;i++)
+	{
+		j=0;
+		n=0;
+		while(d->pole[j]!=0)
+		{
+			if(b->pole[i]==d->pole[j])
+				n=1;
+			
+			j++;
+		}
+		if(n==0)
+		{
+			d->pole[j]=b->pole[i];
+			h++;
+		}
+	}
+	
+	for(i=0;i<h;i++)
+	{
+		printf("%d ",d->pole[i]);
+	}
+}
+
 
 int main(void)
 {
@@ -58,5 +101,10 @@ int main(void)
 	
 	printf("prienik mnozin: ");
 	prienik(&a,&b,&c);
+	
+	printf("\n");
+	
+	printf("zjednotenie mnozin: ");	
+	zjednotenie(&a,&b,&d);	
   
  }
